1b80999d39e8691f537ae533147947a01f457f73
d-sauer <davor.sauer@gmail.com>
2023-04-09 12:00:19 +0200

    Rename API spec folder

--- pkg/api/generate.go
+++ pkg/api/generate.go
@@ -1,4 +1,3 @@
 package api
 
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest --config oapi-codegen.yaml ../../api/DeskController/specification/openapi.yaml
-////go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config oapi-codegen.yaml ../../api/DeskController/specification/openapi.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest --config oapi-codegen.yaml ../../api/desk-controller/specification/openapi.yaml
